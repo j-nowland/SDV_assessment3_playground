211351e408d1bc94ecd682baa67cc942253c55f7
j-nowland <127156607+j-nowland@users.noreply.github.com>
2023-05-29 12:25:29 +1200

    example

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const person1 = {
+    name: 'John',
+    age: 30,
+    greet: function() { console.log('Hi ' + this.name) }
+    password: '1234'
+    email: 'john@gmail.com'
+    address: '1234 Main St'
+    phone: '555-555-5555'
+}
+
