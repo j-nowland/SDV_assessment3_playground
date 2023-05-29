4577823d0f19141c6272a1afbac7766a95430544
j-nowland <127156607+j-nowland@users.noreply.github.com>
2023-05-29 12:35:00 +1200

    checkphone

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const person1 = {
     name: 'John',
-    age: 30,
-    greet: function() { console.log('Hi ' + this.name) }
-    password: '1234'
-    email: 'john@gmail.com'
-    address: '1234 Main St'
-    phone: '555-555-5555'
+    age: '30',
+    password: 'happydays321',
+    email: 'john@gmail.com',
+    address: '1234 Main St',
+    phone: '555-555-5555',
 }
 
+console.log(person1.phone)
+
+// function printName(person1) {
+//     console.log(person1.name)
+// }
+
+// printName(person1)
+
+// function changePassword(person1, newPassword) {
+//     person1.password = newPassword
+// }
+
+// changePassword(person1, 'newPassword')
